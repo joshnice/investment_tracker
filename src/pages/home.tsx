541b553ab2be5c6e624af7eb1e7225e5eca46ff6
Josh Nice <joshliamnice@gmail.com>
2022-04-04 11:32:29 +0400

    Added home buttons

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,23 @@
+import { Button } from "@mui/material";
 import React, { FunctionComponent } from "react";
+import { useNavigate } from "react-router-dom";
+import { Pages } from "../types/global";
 
 const HomeComponent: FunctionComponent = () => {
+    const navigate = useNavigate();
+    const navigateToPage = (page: Pages) => {
+        navigate(page.toLowerCase());
+    }
+
     return (
-        <>
-            HomeComponent
-        </>
+        <div>
+            <h3>Home</h3>
+            <div>
+                <Button variant="outlined" onClick={() => navigateToPage(Pages.OVERVIEW)} >Overview</Button>
+                <Button variant="outlined" onClick={() => navigateToPage(Pages.PURCHASE)}>Purchase</Button>
+                <Button variant="outlined" onClick={() => navigateToPage(Pages.SELL)}>Sell</Button>
+            </div>
+        </div>
     )
 }
 
