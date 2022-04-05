a2b7840c2c717e6eba2876fdeb18c699b40e9a7f
Josh Nice <joshliamnice@gmail.com>
2022-04-05 08:34:05 +0400

    Added home button to not found page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,11 +1,21 @@
+import styled from "@emotion/styled";
 import React, { FunctionComponent } from "react";
+import HomeButtonCompoent from "../components/home-button";
 
 const NotFoundComponent: FunctionComponent = () => {
     return (
-        <>
-            NotFoundComponent
-        </>
+        <HomeButtonContainer>
+            <h3>Page not Found</h3>
+            <HomeButtonCompoent />
+        </HomeButtonContainer>
     )
 }
+
+const HomeButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
 
 export default NotFoundComponent;
