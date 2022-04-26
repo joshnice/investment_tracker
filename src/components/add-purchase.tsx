b9a7710596e979ea958338b0eb34e5616e778946
Josh <joshliamnice@gmail.com>
2022-04-26 17:29:03 +0100

    Added in code input

--- src/components/add-purchase.tsx
+++ src/components/add-purchase.tsx
@@ -26,6 +26,8 @@ const AddPurchaseComponent: FunctionComponent<AddPurchaseProps> = () => {
     // State Values
     const [name, setName] = useState<FormValue<string>>({ value: "", valid: false, message: "", touched: false });
 
+    const [code, setCode] = useState<FormValue<string>>({ value: "", valid: false, message: "", touched: false });
+
     // Handlers
     const handleNameChange = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         const { value } = event.target;
@@ -34,14 +36,28 @@ const AddPurchaseComponent: FunctionComponent<AddPurchaseProps> = () => {
         setName({ value, valid, message, touched: true });
     };
 
+    const handleCodeChange = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const { value } = event.target;
+        const valid = Boolean(value && value.length < 3);
+        const message = valid ? "Code has to be longer than 3 letters" : "";
+        setCode({ value, valid, message, touched: true });
+    };
+
     return (
         <AddPurchaseContainer>
             <TextInputComponent 
-                label="Investment Name" 
+                label="Name" 
                 value={name.value}
                 onChange={handleNameChange} 
                 error={name.touched && name.valid}
                 errorMessage={name.message}
+            />
+            <TextInputComponent 
+                label="Code" 
+                value={code.value}
+                onChange={handleCodeChange} 
+                error={code.touched && code.valid}
+                errorMessage={code.message}
             />
         </AddPurchaseContainer>
     );
@@ -53,6 +69,7 @@ const AddPurchaseContainer = styled.div`
     justify-content: center;
     align-items: left;
     margin: 10px 0px;
+    gap: 20px;
 `;
 
 export default AddPurchaseComponent; 
