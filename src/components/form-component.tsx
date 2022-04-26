790bc408d902ea58e0a0bd99205a28ec718bda41
Josh <joshliamnice@gmail.com>
2022-04-26 15:33:30 +0100

    Styled form buttons

--- src/components/form-component.tsx
+++ src/components/form-component.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Modal } from "@mui/material";
 import { FunctionComponent } from "react";
 
@@ -13,10 +14,19 @@ const FormComponent: FunctionComponent<FormProps> = ({ open, submit, disableSubm
         <DialogTitle>Title</DialogTitle>
         <DialogContent>We need some content</DialogContent>
         <DialogActions>
-            <Button onClick={cancel}>Cancel</Button>
-            <Button onClick={submit} disabled={disableSubmit}>Save</Button>
+            <FormButtonContainer>
+                <Button onClick={cancel} className="form-button">Cancel</Button>
+                <Button onClick={submit} className="form-button" disabled={disableSubmit}>Save</Button>
+            </FormButtonContainer>
         </DialogActions>        
     </Dialog>
 )
+
+const FormButtonContainer = styled.div`
+    padding: 10px;
+    .form-button {
+        width: 100px;
+    }
+`;
 
 export default FormComponent;
