ec72b5d3e1d4ae8612b464aa7294b783cfbfa958
Josh Nice <joshliamnice@gmail.com>
2022-05-01 20:27:38 +0100

    Added select component

---
+++ src/components/forms/select-component.tsx
@@ -0,0 +1,31 @@
+import { Box, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from "@mui/material";
+import React, { FunctionComponent } from "react";
+
+interface SelectItem {
+    value:  string | number;
+    name: string;
+} 
+
+interface SelectButtonProps {
+    title: string;
+    values: SelectItem[];
+    selectedValue: string | number;
+    onChange: (event: SelectChangeEvent<string | number>) => void;
+};
+
+const SelectComponent: FunctionComponent<SelectButtonProps> = ({ title, values, selectedValue, onChange }) => (
+    <Box>
+        <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">{title}</InputLabel>
+            <Select
+                value={selectedValue}
+                label={title}
+                onChange={onChange}
+            >
+                {values.map((val) => <MenuItem key={val.name} value={val.value}>{val.name}</MenuItem> )}
+            </Select>
+        </FormControl>
+    </Box>
+);
+
+export default SelectComponent;
