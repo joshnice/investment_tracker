c42ec0b8bf6c36cc8b5a92e2a49258242f94fba5
Josh Nice <joshliamnice@gmail.com>
2022-05-07 09:23:25 +0100

    Added mapping function

--- src/helper/data-table-helpers.ts
+++ src/helper/data-table-helpers.ts
@@ -1,7 +1,8 @@
 // need function to take the column definitions and use the id from them to get the value out of the row definition
 
 import { ColumnType, PurchaseTableType } from "../types/data-table-types";
-import { PurchaseType } from "../types/global";
+import { FormValue } from "../types/forms";
+import { InvestmentType, PurchaseType } from "../types/global";
 
 export function columnDefinitionToValue(columnDefinitions: ColumnType[], rowData: PurchaseType[]): PurchaseTableType[] {
     return rowData.map((row) => {
@@ -9,4 +10,15 @@ export function columnDefinitionToValue(columnDefinitions: ColumnType[], rowData
         columnDefinitions.forEach((def) => newRowData = { ...newRowData, [def.id]: row[def.id] })
         return newRowData as PurchaseTableType;
     }) 
+}
+
+export function purchaseFormToPurchaseType(formValues: FormValue<string | number | InvestmentType>[]) {
+    let purchase = {};
+    formValues.forEach((formValue) => {
+        purchase = {
+            ...purchase,
+            [formValue.id]: formValue.value,
+        };
+    });
+    return purchase as PurchaseType;
 }
